ac1e8879bef43809b380d8a9464a016c45ea2e8a
Shodmonuz <90080802+Shodmonuz@users.noreply.github.com>
2021-09-12 17:57:11 +0500

    update main.py

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from telebot import TeleBot
-
+from num2words import num2words
 TOKEN = "1978637966:AAEPdNu56x34fz2SsVFlwx6ceS2bnXRA-9A"
 
 bot = TeleBot(TOKEN)
 
 @bot.message_handler(commands=["start"])
 def command_start(message):
-	bot.reply_to(message,"Salom botga xush kelibsiz,")
-	
+	bot.reply_to(message,"Salom botga xush kelibsiz, Menga raqam yuboring.")
+
+@bot.message_handler()
+def habar(message):
+  try:
+    bot.reply_to(message,num2words(message.text))
+  except:
 	
 bot.polling()
